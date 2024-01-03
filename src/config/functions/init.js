f280893792e7f1bddb632fc815d69450ab1b8333
noahSnbr07 <nur.noah.saschenbrecker@gmail.com>
2024-01-03 13:05:29 +0100

    updated initialization for theme preset

--- src/config/functions/init.js
+++ src/config/functions/init.js
@@ -1,14 +1,22 @@
 import './getDate.js';
 import getDate from './getDate.js';
+import Styles from '../../../styles.json';
 export default function init() {
-
     //default set values on boot to prevent runtime errors
     if (localStorage.getItem('score') === null) { localStorage.setItem('score', 0); }
     if (localStorage.getItem('score_alltime') === null) { localStorage.setItem('score_alltime', 0); }
     if (localStorage.getItem('date') === null) { localStorage.setItem('date', getDate()); }
+    if (localStorage.getItem('theme') === null) { localStorage.setItem('theme', 0) }
 
     //reset score if new day approaches
-    if (localStorage.getItem('date') != getDate()) {
-        localStorage.setItem('score', 0);
-    }
+    if (localStorage.getItem('date') != getDate()) { localStorage.setItem('score', 0); }
+
+    //preset :root styles
+    const currentStyle = Styles[localStorage.getItem('theme')].set;
+    document.documentElement.style.setProperty('--bg-1', currentStyle["--bg-1"]);
+    document.documentElement.style.setProperty('--bg-2', currentStyle["--bg-2"]);
+    document.documentElement.style.setProperty('--bg-3', currentStyle["--bg-3"]);
+    document.documentElement.style.setProperty('--fn-1', currentStyle["--fn-1"]);
+    document.documentElement.style.setProperty('--fn-2', currentStyle["--fn-2"]);
+
 }
