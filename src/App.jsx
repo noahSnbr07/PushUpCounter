55a1c05391360b5019e7d0834f17d70cdb658f40
noahSnbr07 <nur.noah.saschenbrecker@gmail.com>
2024-01-16 16:30:10 +0100

    new index route solution+

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 /* eslint-disable react/prop-types */
-import { Routes, Route, useResolvedPath, useMatch, Link } from 'react-router-dom';
+import { Routes, Route, useResolvedPath, useMatch, Link, useNavigate } from 'react-router-dom';
 
 //import components
 import Icon from './config/components/Icon.jsx';
 import Today from './pages/Today';
 import Settings from './pages/Settings';
+import { useEffect } from 'react';
 
 export default function App() {
+    const navigate = useNavigate();
+
+    useEffect(() => { navigate('today'); }, []);
 
     return (
         <main className='App'>
             <div className='viewport'>
                 <Routes>
-                    <Route index element={<Today />} />
+                    {/* <Route index element={<Today />} /> */}
                     <Route path='today' element={<Today />} />
                     <Route path='settings' element={<Settings />} />
                 </Routes>
